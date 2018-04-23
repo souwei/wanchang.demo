ba5c0e916243c3f10d1d9928ae87c6f94c4f64bd
souwei <sou.wei.wong@gmail.com>
2018-04-23 17:10:56 +0800

    configured app to use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20180422152554) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -60,7 +63,7 @@ ActiveRecord::Schema.define(version: 20180422152554) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "patient_id"
+    t.bigint "patient_id"
     t.index ["patient_id"], name: "index_patient_notes_on_patient_id"
   end
 
@@ -85,4 +88,5 @@ ActiveRecord::Schema.define(version: 20180422152554) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "patient_notes", "patients"
 end
