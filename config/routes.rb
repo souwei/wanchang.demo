ffd956e7246b2546488270e8a319923c5aac691a
souwei <sou.wei.wong@gmail.com>
2018-04-23 17:29:31 +0800

    changed patients to be home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :patient_notes
   end
 
+  root :to => "patients#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
