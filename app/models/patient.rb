6d889322a12cf9f85e0a54bc3b214a0cda6a7dd6
souwei <sou.wei.wong@gmail.com>
2018-04-24 13:06:27 +0800

    ordered patients display alphabetically by default

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ApplicationRecord
   searchkick
   validates_presence_of :given_name, :date_of_birth, :contact_number
   has_many :patient_notes
+  default_scope { order(given_name: :asc) }
 end
