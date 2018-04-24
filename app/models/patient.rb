f2586a6ddfe3115b655e0c69be6fc8c7c4eaa01a
souwei <sou.wei.wong@gmail.com>
2018-04-24 19:27:17 +0800

    clean up patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ApplicationRecord
   searchkick
   validates_presence_of :given_name, :date_of_birth, :contact_number
-  has_many :patient_notes
+  has_many :patient_notes, dependent: :destroy
   default_scope { order(given_name: :asc) }
 end
